8d44313d477e26d3db73af53d746abced8250733
ilham-bennecib <ilham86@live.fr>
2023-03-20 00:41:36 +0100

    import sampleText destructuring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {sampleText} from './sampleText';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm-6'>
+          <textarea
+          value ={sampleText} 
+          className='form-control' row = '35'></textarea>
+        </div>
+        <div className='col-sm-6'>
+          <div> {sampleText} </div>
+        </div>
+      </div>
     </div>
   );
 }
